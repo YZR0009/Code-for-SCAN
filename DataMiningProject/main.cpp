da6a308bd427a279296fb05fb25c835d4f7b3385
YZR0009 <yzr0009@tigermail.auburn.edu>
2017-11-26 11:33:39 -0600

    Initial Commit

---
+++ DataMiningProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataMiningProject
+//
+//  Created by Yunfan Rao on 11/26/17.
+//  Copyright © 2017 Yunfan Rao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
